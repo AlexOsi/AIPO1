5a5874deb37e58e4280b5b48cde7f3bbf72d63ab
Alex Osi <alexosinstev.job@gmail.com>
2021-04-20 19:48:47 +0200

    Init

---
+++ cmake-build-debug/CMakeFiles/AIPO1.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "AIPO1.exe"
+  "AIPO1.exe.manifest"
+  "AIPO1.lib"
+  "AIPO1.pdb"
+  "CMakeFiles/AIPO1.dir/main.cpp.obj"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/AIPO1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
